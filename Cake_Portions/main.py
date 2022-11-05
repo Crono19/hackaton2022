16b25d9a9749ecfdd6ae77b91484164bec3860bc
Santi Urbina <urbinasanti1@gmail.com>
2022-11-05 15:32:21 -0600

    fourth problem update
    the information has been updated to make it more visually appealing.

--- Cake_Portions/main.py
+++ Cake_Portions/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def main():
     friends = int(input('Por favor ingrese la cantidad de amigos que tiene: '))
     portions = int(input('Por favor ingrese la cantidad de porciones que desean sus amigos: '))
@@ -8,7 +11,7 @@ def cake_slices(friends, portions):
     total_portions = friends * portions
     total_cakes = total_portions / 4
 
-    return f'Se necesita un total de {total_cakes} pasteles para satisfacer la cantidad de porciones requeridas'
+    return f'Se necesita un total de {math.ceil(total_cakes)} pasteles para satisfacer la cantidad de porciones requeridas'
 
 
 main()
